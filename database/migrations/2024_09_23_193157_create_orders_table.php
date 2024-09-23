66ff60bf1c5ef7a2a0f863eecfc870bf0bf0b6a3
shahariarrahman1 <rshahariar758@gmail.com>
2024-09-24 01:45:05 +0600

    ecommere project

---
+++ database/migrations/2024_09_23_193157_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('order_number', 50)->unique();
+            $table->foreignId('address_id')->constrained('addresses')->onDelete('cascade');
+            $table->decimal('total_price', 8, 2);
+            $table->string('payment_method');
+            $table->string('status')->default('Pending');
+            $table->string('payment_status')->default('pending_payment'); // This line is already included
+            $table->string('transaction_id')->nullable();
+            $table->string('currency')->default('BDT');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
